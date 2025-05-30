b795c3d15e34533dc847f792b7ce60c640e1b93c
TomNaberink <141318593+TomNaberink@users.noreply.github.com>
2025-05-30 15:36:19 +0200

    Update webpack configuration to ignore pdf-parse test files

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(
-        new config.webpack.IgnorePlugin({
-          resourceRegExp: /\.pdf$/,
-          contextRegExp: /pdf-parse\/test\/data/,
-        })
-      );
-    }
-    return config;
-  }
+  devIndicators: false
 }
 
-module.exports = nextConfig
+module.exports = nextConfig 
